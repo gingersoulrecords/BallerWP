408dad813bd5174feae0556718f6740978388e52
Soul Plugins <gingersoulrecords@gmail.com>
2024-06-08 10:49:36 -0400

    defer checkbox

--- baller.php
+++ baller.php
@@ -9,6 +9,7 @@ function ballerwp_menu()
 {
     add_options_page('BallerWP Settings', 'BallerWP', 'manage_options', 'ballerwp', 'ballerwp_options');
 }
+
 function ballerwp_options()
 {
     if (!current_user_can('manage_options')) {
@@ -18,8 +19,10 @@ function ballerwp_options()
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $scripts = $_POST['scripts'];
         $dependency = $_POST['dependency'];
+        $defer = isset($_POST['defer']) ? '1' : '0';
         update_option('ballerwp_scripts', $scripts);
         update_option('ballerwp_dependency', $dependency);
+        update_option('ballerwp_defer', $defer);
         $urls = explode("\n", str_replace("\r", "", $scripts));
         $ball = '';
         foreach ($urls as $url) {
@@ -38,16 +41,19 @@ function ballerwp_options()
     } else {
         $scripts = get_option('ballerwp_scripts', '');
         $dependency = get_option('ballerwp_dependency', '');
+        $defer = get_option('ballerwp_defer', '0');
     }
 
     echo '<div class="wrap">';
     echo '<form method="post">';
     echo '<textarea name="scripts" rows="10" cols="50">' . esc_textarea($scripts) . '</textarea>';
     echo '<input type="text" name="dependency" value="' . esc_attr($dependency) . '" placeholder="Dependency handle" />';
+    echo '<label><input type="checkbox" name="defer" value="1"' . checked('1', $defer, false) . ' /> Defer script</label>';
     echo '<input type="submit" value="Submit" />';
     echo '</form>';
     echo '</div>';
 }
+
 function ballerwp_enqueue_scripts()
 {
     $ball_file = plugin_dir_url(__FILE__) . 'ball.js';
@@ -55,3 +61,24 @@ function ballerwp_enqueue_scripts()
     wp_enqueue_script('ballerwp_ball', $ball_file, array($dependency), false, true);
 }
 add_action('wp_enqueue_scripts', 'ballerwp_enqueue_scripts');
+
+function ballerwp_add_defer_attribute($tag, $handle)
+{
+    // If the handle isn't 'ballerwp_ball', return the tag unmodified
+    if ('ballerwp_ball' !== $handle) {
+        return $tag;
+    }
+
+    // If the 'defer' option is not set, return the tag unmodified
+    if (get_option('ballerwp_defer', '0') !== '1') {
+        return $tag;
+    }
+
+    // If the 'defer' attribute is not present, add it
+    if (false === strpos($tag, ' defer ')) {
+        $tag = str_replace(' src', ' defer src', $tag);
+    }
+
+    return $tag;
+}
+add_filter('script_loader_tag', 'ballerwp_add_defer_attribute', 10, 2);
